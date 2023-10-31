9bd9811a9540b2feb553841d973327e7a6ee1e1c
Martin Jul <martin@mjul.com>
2023-10-31 22:41:31 +0100

    Proxy request to server API

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Trying CodeWhisperer LLM coding assistant to build a TODO list app in Rust and T
 - CW likes to suggest modules even they they are not used
 - CW does not work in the `Cargo.toml` file
 
+You can run the server like this:
+
+    cargo run
+
 
 ## Client
 To give CW a skeleton to infer from, I created the client application with `create-react-app` like this:
@@ -13,3 +17,13 @@ To give CW a skeleton to infer from, I created the client application with `crea
     npx create-react-app client --template typescript
 
 I also added the Ant Design component library, `npm install antd --save`.
+
+You can run the client in development mode like this:
+
+    npm start
+
+The development environment is set up to proxy to the Rust backend, see https://create-react-app.dev/docs/proxying-api-requests-in-development
+The corresponding line is the `"proxy"` key in `package.json`.
+
+
+
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Alert, Spin, Switch } from 'antd';
+
+const {useState, useEffect } = React;
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    // fetch for type application/json
+    fetch(`/api/todos`)
+      .then((response) => {
+        console.log(response);
+        setLoading(false);
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setError(null);
+        setTodos(actualData);
+        console.log(actualData)
+      })
+      .catch((err) => {
+        setError(err.message);
+        setTodos([]);
+        console.log(err.message);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Layout.Header>
+        Header
+      </Layout.Header>
+      <Layout.Content>
+        <Spin spinning={loading}>
+          <Alert
+            message="Loading TODOs..."
+            description="Requesting data from the server..."
+            type="info"
+          />
+        </Spin>
+      </Layout.Content>
+      <Layout.Footer>
+        Built with an LLM.
+      </Layout.Footer>
+    </Layout>
   );
 }
+
 
 export default App;
